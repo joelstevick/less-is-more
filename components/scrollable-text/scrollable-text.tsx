f90c67cfce1f850e3de5d22d3200202a11cc965f
Joel Stevick <joelstevick@outlook.com>
2024-06-01 08:43:06 -0400

    add scrollable text component

---
+++ components/scrollable-text/scrollable-text.tsx
@@ -0,0 +1,32 @@
+// components/ScrollableText.js
+import React from "react";
+
+const ScrollableText = () => {
+  return (
+    <div className="p-4 border border-blue-300 dark:border-blue-700 rounded-lg shadow-sm bg-blue-100 dark:bg-blue-800 text-blue-900 dark:text-blue-100 overflow-y-auto">
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a dictum
+        nunc. Curabitur non felis eu eros commodo pellentesque. Sed mollis lorem
+        ac justo feugiat, ac tincidunt libero viverra. Donec vehicula dapibus
+        eros, vel feugiat est ultrices a. Etiam ut mauris ac urna vulputate
+        efficitur a nec eros. Donec eu risus eget nisl condimentum ultrices. Sed
+        at dui sit amet justo bibendum luctus. Curabitur egestas bibendum dui,
+        sed facilisis lorem lobortis in. Integer dictum lacus id sagittis
+        consectetur. Aenean nec feugiat nunc. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas.
+      </p>
+      <p className="mb-4">
+        Cras a nunc odio. Ut consequat enim eu neque finibus, nec vestibulum
+        odio feugiat. Proin id est at turpis facilisis lacinia. Donec nec diam
+        sit amet lacus tincidunt scelerisque ac a sapien. Vivamus aliquam lacus
+        ut dictum bibendum. Curabitur at semper ante. Ut pharetra nulla id
+        sollicitudin dapibus. Sed eu elit eget nulla sagittis fermentum ac sit
+        amet justo. Etiam in ex hendrerit, placerat libero vel, dictum risus.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas.
+      </p>
+    </div>
+  );
+};
+
+export default ScrollableText;
