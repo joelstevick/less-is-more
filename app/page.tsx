73f3e29c9ae9ed9e5bddc9efddeb96f7274e91f2
Joel Stevick <joelstevick@outlook.com>
2024-06-01 16:37:00 -0400

    reduce life of toast message

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
       {loading && <Spinner />}
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
       />
     </main>
