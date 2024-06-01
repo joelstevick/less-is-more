4b2d64a48646d9103fc22278da94dbc827d86d75
Joel Stevick <joelstevick@outlook.com>
2024-06-01 10:35:03 -0400

    add HSpacer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Textarea from "@/components/textarea/textarea";
 import ScrollableText from "@/components/scrollable-text/scrollable-text";
@@ -6,6 +6,7 @@ import Button from "@/components/button/button";
 import VSpacer from "@/components/v-spacer/v-spacer";
 import { useRef } from "react";
 import CopyToClipboard from "@/components/clipboard/copy-to-clipboard";
+import HSpacer from "@/components/h-spacer/h-spacer";
 
 export default function Home() {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -14,11 +15,11 @@ export default function Home() {
     <main className="p-16">
       <div className="flex justify-end">
         <Button>Reduce</Button>
+        <HSpacer />
         <CopyToClipboard textareaRef={textareaRef} />
-
       </div>
       <VSpacer />
-      <Textarea ref={textareaRef}/>
+      <Textarea ref={textareaRef} />
 
       <VSpacer />
       <VSpacer />
