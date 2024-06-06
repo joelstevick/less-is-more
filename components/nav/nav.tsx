428bb0b331461af11b05db9846d39e496b2e8549
Joel Stevick <joelstevick@outlook.com>
2024-06-06 08:39:11 -0400

    change color of nav bar

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Link from "next/link"; // Import Link component from Next.js
+import HSpacer from "../h-spacer/h-spacer";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-end bg-gray-200 py-4 px-6 text-2xl">
-      <ul className="flex space-x-4 text-gray-500">
-        <li className=" hover:text-gray-900">
+    <nav className="flex justify-end bg-blue-500 py-4 px-6 text-2xl">
+      <ul className="flex space-x-4 text-white">
+        <li className=" hover:text-blue-100">
           <Link href="/">Home</Link>
         </li>
-        <li className=" hover:text-gray-900">
+        <li><HSpacer /></li>
+        <li className=" hover:text-blue-100">
           <Link href="/history">History</Link>
         </li>
       </ul>
