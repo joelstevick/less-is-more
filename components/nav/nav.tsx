edf747800d2379cc5a44fe9f5bf76207a8914675
Joel Stevick <joelstevick@outlook.com>
2024-06-06 20:38:13 -0400

    use a more standard approach to making sure that the router is activated

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,30 +1,23 @@
-"use client"
-import React, { useEffect, useState } from "react";
+"use client";
+import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Import usePathname from Next.js
 import HSpacer from "../h-spacer/h-spacer";
 
 const Nav = () => {
   const pathname = usePathname();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null; // Ensures the component is mounted
 
   return (
     <nav className="flex justify-end bg-blue-500 py-4 px-6 text-2xl">
-      <ul className="flex space-x-4 text-blue-300">
+      <ul className="flex space-x-4 text-white">
         <li
-          className={`hover:text-blue-100 ${pathname === '/' ? 'text-white' : ''}`}
+          className={`hover:text-blue-100 ${pathname === '/' ? 'text-blue-300' : ''}`}
         >
           <Link href="/">Home</Link>
         </li>
         <li><HSpacer /></li>
         <li
-          className={`hover:text-blue-100 ${pathname === '/history' ? 'text-white' : ''}`}
+          className={`hover:text-blue-100 ${pathname === '/history' ? 'text-blue-300' : ''}`}
         >
           <Link href="/history">History</Link>
         </li>
