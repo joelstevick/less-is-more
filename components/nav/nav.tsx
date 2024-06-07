f8df51bb7c330951bdb6ce6046b7b2d11ccec8b1
Joel Stevick <joelstevick@outlook.com>
2024-06-06 20:15:43 -0400

    use state for tabs

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Link from "next/link"; // Import Link component from Next.js
 import HSpacer from "../h-spacer/h-spacer";
+import { SelectedTab } from "../global-context/global-context";
 
 const Nav = () => {
+  const [selectedTab, setSelectedTab] = useState(SelectedTab.home)
+
   return (
     <nav className="flex justify-end bg-blue-500 py-4 px-6 text-2xl">
       <ul className="flex space-x-4 text-white">
