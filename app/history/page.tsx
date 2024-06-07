fadd37b50bcdc21f6bdb02e76b31db9a1de149fe
Joel Stevick <joelstevick@outlook.com>
2024-06-07 18:19:50 -0400

    add server side auth guard

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,7 +1,12 @@
+import ServerAuthGuard from "../guards/server-auth0-guard";
 import { supabase } from "../supabase/server";
 
 export default async function HistoryPage() {
   const { data: history } = await supabase.from("history").select();
 
-  return <main className="p-16 h-screen">{JSON.stringify(history, null, 2)}</main>
+  return (
+    <ServerAuthGuard>
+      <main className="p-16 h-screen">{JSON.stringify(history, null, 2)}</main>
+    </ServerAuthGuard>
+  );
 }
