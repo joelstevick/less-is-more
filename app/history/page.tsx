fe9f18dc2b3d60571eda1b4e468347e32b14f2a4
Joel Stevick <joelstevick@outlook.com>
2024-06-11 08:41:16 -0400

    add nav to logout and history

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,9 +1,13 @@
+import Nav from "@/components/nav/nav";
 import { supabase } from "../supabase/server";
 
 export default async function HistoryPage() {
   const { data: history } = await supabase.from("history").select();
 
   return (
+    <>
+      <Nav />
       <main className="p-16 h-screen">{JSON.stringify(history, null, 2)}</main>
+    </>
   );
 }
--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Button from "@/components/button/button";
 import Spinner from "@/components/spinner/spinner";
+import Nav from "@/components/nav/nav";
 
 export default function Logout() {
   const router = useRouter();
@@ -11,19 +12,19 @@ export default function Logout() {
 
   const handleLogout = async () => {
     setLoading(true);
-    const res = await fetch('/api/logout', {
-      method: 'POST',
+    const res = await fetch("/api/logout", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     setLoading(false);
     if (res.ok) {
-      router.push('/login');
+      router.push("/login");
     } else {
       const data = await res.json();
-      console.error('Error logging out:', data.error);
+      console.error("Error logging out:", data.error);
     }
   };
 
@@ -32,17 +33,22 @@ export default function Logout() {
   };
 
   return (
-    <main className="p-16 h-screen flex flex-col items-center justify-center">
-      <div className="text-blue-500 text-4xl text-center mb-16">Logout</div>
-      <div className="text-center mb-8">Are you sure you want to log out?</div>
-      <div className="flex space-x-4">
-        <Button onClick={handleLogout} disabled={loading}>
-          {loading ? <Spinner /> : 'Yes, Log me out'}
-        </Button>
-        <Button onClick={handleCancel} disabled={loading}>
-          No, Cancel
-        </Button>
-      </div>
-    </main>
+    <>
+      <Nav></Nav>
+      <main className="p-16 h-screen flex flex-col items-center justify-center">
+        <div className="text-blue-500 text-4xl text-center mb-16">Logout</div>
+        <div className="text-center mb-8">
+          Are you sure you want to log out?
+        </div>
+        <div className="flex space-x-4">
+          <Button onClick={handleLogout} disabled={loading}>
+            {loading ? <Spinner /> : "Yes, Log me out"}
+          </Button>
+          <Button onClick={handleCancel} disabled={loading}>
+            No, Cancel
+          </Button>
+        </div>
+      </main>
+    </>
   );
 }
